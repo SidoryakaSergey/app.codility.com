81de912f92db40b3f077e38fde02fae5f93bc93b
Thanatus <s.sidoryaka@gmail.com>
2020-10-26 14:50:44 +0200

    add l2

--- lesson2_OddOccurrencesInArray.js
+++ lesson2_OddOccurrencesInArray.js
@@ -1,26 +1,7 @@
 const A = [9, 9, 3, 5, 3, 7, 7];
 
 function solution(A) {
-  let N = 0;
-  let len = A.length - 1;
-  if (!len) {
-    return A[0];
-  }
-  for (let i = len; i >= 0; i--) {
-    if (A[i] === null) {
-      //   A.pop();
-      i--;
-    }
-    let temp = A[i];
-    // A.pop();
-    A[i] = null;
-    let Index = A.indexOf(temp);
-    if (Index > -1) {
-      A[Index] = null;
-    } else {
-      return temp;
-    }
-  }
+  return Object.assign({}, [1, 3, 4]);
 }
 
 console.log(solution(A));
